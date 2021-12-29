3aad1ccdc1af9f641efcc008e5971a1030d4e2dd
nerite <faulttester@gmail.com>
2021-12-30 00:26:19 +0200

    Practiced more star and number patterns

--- src/test/java/loopsConditions/NumberPatterns.java
+++ src/test/java/loopsConditions/NumberPatterns.java
@@ -74,6 +74,57 @@ public class NumberPatterns {
 			System.out.println();
 		}
 		System.out.println();
+		
+		/**
+		 *  1
+		 *  2 2
+		 *  3 3 3
+		 *  4 4 4 4
+		 *  5 5 5 5 5
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		/**
+		 *      1
+		 *     1 2
+		 *    1 2 3
+		 *   1 2 3 4
+		 *  1 2 3 4 5    
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print(k+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		/**
+		 *  1 2 3 4 5
+		 *  1 2 3 4
+		 *  1 2 3
+		 *  1 2
+		 *  1
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=6-i; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 
 	}
 
--- src/test/java/loopsConditions/StarPatterns.java
+++ src/test/java/loopsConditions/StarPatterns.java
@@ -38,6 +38,25 @@ public class StarPatterns {
 			System.out.println();
 		}
 		System.out.println();
+		
+		/**
+		 *      *
+		 *     ***
+		 *    *****
+		 *   *******
+		 *  *********    
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i+(i-1); k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
 
 	}
 
