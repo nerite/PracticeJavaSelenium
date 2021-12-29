486bad3b9e347bf5636172f32cb240557d689761
nerite <faulttester@gmail.com>
2021-12-29 16:46:09 +0200

    Practiced some star and number patterns for loops

---
+++ src/test/java/loopsConditions/NumberPatterns.java
@@ -0,0 +1,80 @@
+package loopsConditions;
+
+public class NumberPatterns {
+
+	public static void main(String[] args) {
+		
+		/**
+		 *  1
+		 *  1 2
+		 *  1 2 3
+		 *  1 2 3 4
+		 *  1 2 3 4 5
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		/**
+		 *          1
+		 *        1 2
+		 *      1 2 3 
+		 *    1 2 3 4
+		 *  1 2 3 4 5        
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5-i; j++) {
+				System.out.print("  ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print(k+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		/**
+		 *  5
+		 *  4 5
+		 *  3 4 5
+		 *  2 3 4 5
+		 *  1 2 3 4 5
+		 */
+		
+		for(int i=5; i>=1; i--) {
+			for(int j=i; j<=5; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+				
+		
+		/**
+		 *          5
+		 *        4 5
+		 *      3 4 5
+		 *    2 3 4 5
+		 *  1 2 3 4 5        
+		 */
+		
+		for(int i=5; i>=1; i--) {
+			for(int j=i-1; j>=1; j--) {
+				System.out.print("  ");
+			}
+			for(int k=i; k<=5; k++) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+
+}
---
+++ src/test/java/loopsConditions/StarPatterns.java
@@ -0,0 +1,44 @@
+package loopsConditions;
+
+public class StarPatterns {
+
+	public static void main(String[] args) {
+		
+		/**
+		 *  *
+		 *  * *
+		 *  * * *
+		 *  * * * *
+		 *  * * * * *
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		/**
+		 *           *
+		 *         * *
+		 *       * * *
+		 *     * * * *
+		 *   * * * * *        
+		 */
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5-i; j++) {
+				System.out.print("  ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+
+}
