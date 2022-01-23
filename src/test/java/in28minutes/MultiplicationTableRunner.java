5d55d3db437cd1847ecce76910271da902e92398
nerite <faulttester@gmail.com>
2022-01-23 22:36:58 +0200

    Created MultiplicationTableRunner with main method and new object table

---
+++ src/test/java/in28minutes/MultiplicationTableRunner.java
@@ -0,0 +1,14 @@
+package in28minutes;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+		//table.print(6);
+		//table.print(8, 11, 20);
+
+	}
+
+}
