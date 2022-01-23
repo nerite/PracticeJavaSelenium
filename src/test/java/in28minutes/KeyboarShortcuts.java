87cb1614c82ef00673f316c3b39abe89f710d321
nerite <faulttester@gmail.com>
2022-01-23 22:41:18 +0200

    Created KeyboardShortcuts using just keyboard shortcuts

---
+++ src/test/java/in28minutes/KeyboarShortcuts.java
@@ -0,0 +1,11 @@
+package in28minutes;
+
+public class KeyboarShortcuts {
+	
+	public static void main(String[] args) {
+		System.out.println("main ctrl + space - writes main method");
+		System.out.println("sysout ctrl + space - writes System.out.println()");
+		System.out.println("shift + ctrl + L - opens shortcuts list");
+	}
+
+}
