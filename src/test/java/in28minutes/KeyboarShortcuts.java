1dc5c8d16ff545ccd4c1ca545e86dc8e9aa974e4
Neringa Slezeviciene <faulttester@gmail.com>
2022-10-18 17:45:39 +0300

    added intellij ide shortcut

--- src/test/java/in28minutes/KeyboarShortcuts.java
+++ src/test/java/in28minutes/KeyboarShortcuts.java
@@ -3,9 +3,13 @@ package in28minutes;
 public class KeyboarShortcuts {
 	
 	public static void main(String[] args) {
+		System.out.println("-----------Eclipse IDE--------------");
 		System.out.println("main ctrl + space - writes main method");
 		System.out.println("sysout ctrl + space - writes System.out.println()");
 		System.out.println("shift + ctrl + L - opens shortcuts list");
+
+		System.out.println("\n-----------IntelliJ IDE-------------");
+		System.out.println("sout Enter or Tab - writes System.out.println();");
 	}
 
 }
