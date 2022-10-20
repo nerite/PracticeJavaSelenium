145cc864f3c5bf2c7f5909f780c5a7c55bd67618
Neringa Slezeviciene <faulttester@gmail.com>
2022-10-20 18:07:12 +0300

    upgraded java to 11 version and added scanner object

--- src/test/java/controlStructureInJava/UserInput.java
+++ src/test/java/controlStructureInJava/UserInput.java
@@ -1,4 +1,21 @@
 package controlStructureInJava;
 
+import java.util.Scanner;
+
 public class UserInput {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter the temperature in Fahrenheit: ");
+        Scanner tempInput = new Scanner(System.in);
+        //int temperature = tempInput.nextInt();
+        float temperature = tempInput.nextFloat();
+
+        // double temperature = tempInput.nextDouble();   // doesn't let to enter double value
+        System.out.println("The input temperature is : " + temperature);
+
+    }
+
 }
+
+
