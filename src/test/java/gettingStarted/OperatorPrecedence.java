37ef67ef9afdf54c6197a5fb7a01e10b7926efb6
Neringa Slezeviciene <faulttester@gmail.com>
2022-10-20 11:53:09 +0300

    Added exapmles of operators who has priorities

--- src/test/java/gettingStarted/OperatorPrecedence.java
+++ src/test/java/gettingStarted/OperatorPrecedence.java
@@ -1,4 +1,29 @@
 package gettingStarted;
 
 public class OperatorPrecedence {
+
+    public static void main(String[] args) {
+
+        double a = 2, b = 5, c = 6, d = 10;
+
+        System.out.println("a + b * d = " + (a + b * d));
+        System.out.println("(a + b) * d = " + (a + b) * d);
+        System.out.println("a + (b * d) = " + (a + (b * d)));
+
+//        a + b * d = 52.0
+//        (a + b) * d = 70.0
+//        a + (b * d) = 52.0
+
+        System.out.println("a + b / d = " + (a + b / d));
+        System.out.println("(a + b) / d = " + ((a + b) / d));
+        System.out.println("a + b / d - c = " + (a + b / d - c));
+        System.out.println("a + b / (d - c) = " + (a + b / (d - c)));
+
+//        a + b / d = 2.5
+//        (a + b) / d = 0.7
+//        a + b / d - c = -3.5
+//        a + b / (d - c)3.25
+
+    }
+
 }
